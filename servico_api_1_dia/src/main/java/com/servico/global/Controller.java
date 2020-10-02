a70afd4d43c2832afe9803b185fb76fe7d1e00e0
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2020-10-02 19:06:05 -0300

    Exemplo plataforma

--- servico_api_1_dia/src/main/java/com/servico/global/Controller.java
+++ servico_api_1_dia/src/main/java/com/servico/global/Controller.java
@@ -3,26 +3,51 @@ package com.servico.global;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/servicos")
 public class Controller {
 
 	@Autowired
 	private ServicoRepository repository;
 
-	@GetMapping("/servico")
-	public List<ServicoModel> PegarTodos() {
-
+	@GetMapping
+	public List<ServicoModel> index() {
 		return repository.findAll();
-
 	}
 
-	@PostMapping("/servico")
-	public ServicoModel criar(@RequestBody ServicoModel model) {
+	@PostMapping
+	public ServicoModel create(@RequestBody ServicoModel servico) {
+		return repository.save(servico);
+	}
+	
+	@PutMapping
+	public ServicoModel update(@RequestBody ServicoModel servico) {
+		return repository.save(servico);
+	}
+	
+	@DeleteMapping
+	public String delete(@RequestBody ServicoModel servico) {
+		repository.delete(servico);
+		return "Serviço deletado";
+	}
+	
+	@GetMapping("/servico/nome/{nome}")
+	public List<ServicoModel> buscarPorNome(@PathVariable String nome) {
+		return repository.findByNome(nome);
+	}
+
+	@PutMapping("/servicos/{id}")
+	public ServicoModel atualizar(@PathVariable Long id, @RequestBody ServicoModel model) {
+		model.setId(id);
 		repository.save(model);
 		return model;
 	}
