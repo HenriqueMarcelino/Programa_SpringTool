f47aff30d8a5b15f3db2e8ed01a8289ff91e6fbd
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2020-10-13 01:18:17 -0300

    Deletado

--- metodo_save/src/main/java/com/metodosave/global/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.metodosave.global;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<TableProduto, Long>{
-	
-}
